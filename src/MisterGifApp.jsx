48c0a38e2a8aa3e45557c293f0d4287135013b3e
Saul Castillo <secch97@gmail.com>
2023-02-27 20:00:07 -0500

    Mister GIF app creation - Basic structure

---
+++ src/MisterGifApp.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MisterGifApp = () => {
+  return (
+    <>
+        {/* Title */}
+        <h1>Mister GIF</h1>
+        
+        {/* Input */}
+
+        {/* GIF list */}
+            {/* GIF Items */}
+
+    </>
+  );
+};
+
+export {
+    MisterGifApp
+};
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MisterGifApp } from './MisterGifApp';
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MisterGifApp />
+  </React.StrictMode>,
+);
