4ed62aee5b9e8871074e2ac28590f6e0918b6d80
Saul Castillo <secch97@gmail.com>
2023-03-05 21:10:20 -0500

    Finished custom hook creation. Added spme css styling

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const GifItem = ({category, title, url}) => {
-    console.log(title, url);
   return (
     <div className='card'>
         <img src={url} alt={title}/>
-        <p>{title.trim() ? title:`${category} GIF`}</p>
+        <span>{title.trim() ? title:`${category} GIF`}</span>
     </div>
   )
 };
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,10 +1,12 @@
 const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=FWCszwwyG9YJM5R3VACQkGLUQBl2M7Qe&q=${category}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=FWCszwwyG9YJM5R3VACQkGLUQBl2M7Qe&q=${category}&limit=12`;
     const response = await fetch(url, {
         method: "GET",
         mode: "cors"
     });
     const {data} = await response.json();
+    console.log(data);
+
     const gifs = data.map((gif) => {
         return {
             id: gif.id,
