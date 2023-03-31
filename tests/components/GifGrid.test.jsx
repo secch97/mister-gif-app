180aa9238dc71e6a28b07889fa6229eee757d880
Saul Castillo <secch97@gmail.com>
2023-03-31 12:21:44 -0400

    Complete mock of a custom hook

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,10 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('<GidGrid/> Testing.', () => { 
     const category = "One punch";
 
     test('1. Should show at first a loading screen' , () => { 
+        useFetchGifs.mockReturnValue({
+            gifs: [],
+            isLoading: true
+        });
         render(<GifGrid category={category} firstPosition={false}/>);
         screen.debug();
         expect(screen.getByText("Loading...")).toBeTruthy();
@@ -12,6 +19,24 @@ describe('<GidGrid/> Testing.', () => {
      });
 
      test('2. Should show items when gifs are loaded', () => { 
+       const gifs = [
+        {
+            id: "ABC",
+            title: "Saitama",
+            url: "https://localhost/saitama.png"
+        },
+        {
+            id: "DEF",
+            title: "Goku",
+            url: "https://localhost/goku.png"
+        }
+       ];
 
+        useFetchGifs.mockReturnValue({
+            gifs: gifs,
+            isLoading: false
+        });
+        render(<GifGrid category={category} firstPosition={false}/>);
+        expect(screen.getAllByRole('img').length).toBe(2);
       });
  });
